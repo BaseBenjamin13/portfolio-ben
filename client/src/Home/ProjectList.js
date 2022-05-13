3cbcb7c1cb1d0eb266bdaa9a577f2819e1620391
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-05-13 00:49:19 -0400

    some project listing style

--- client/src/Home/ProjectList.js
+++ client/src/Home/ProjectList.js
@@ -9,6 +9,12 @@ function ProjectList() {
         <div className="project">
             <h1>Project 2</h1>
         </div>
+        <div className="project">
+            <h1>Project 3</h1>
+        </div>
+        <div className="project">
+            <h1>Project 4</h1>
+        </div>
     </div>
   )
 }
--- client/src/StartPage/StartPage.js
+++ client/src/StartPage/StartPage.js
@@ -9,7 +9,7 @@ function StartPage() {
         {/* Link is better then a href because it dosent send unneeded request to server, 
         it just loads the html, making it load faster. */}
 		<div className="div-mywork">
-        <h1 className="welcome">Welcome To Ben's</h1> 
+        <h1 className="welcome">Welcome To Ben's Portfolio</h1> 
 			<Link to='/home' ><button className="work-btn">My Work</button></Link>
 		</div>
     
