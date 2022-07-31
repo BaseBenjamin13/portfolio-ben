8d9adc26cdac322fce2f6e9e9661c8a8414d3039
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-07-30 22:56:11 -0400

    setup lang icons

--- client/src/components/LangsAndDatabases.js
+++ client/src/components/LangsAndDatabases.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function LangsAndDatabases() {
+
+    const [icons, setIcons] = useState([
+        {
+            title: "JavaScript",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+            alt: "javascript icon"
+        },
+        {
+            title: "Python",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+            alt: "python icon"
+        },
+        {
+            title: "HTML",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+            alt: "html icon"
+        },
+        {
+            title: "CSS",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+            alt: "css icon"
+        },
+        {
+            title: "PSQL",
+            svg: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+            alt: "sql icon"
+        },
+    ])
+
   return (
     <div className="langs-container">
         <h1 className="about-me-title">Languages and Databases</h1>
+        <div className="icons-container">
+            {
+                icons.map(icon => {
+                    return(
+                        <div>
+                            <img src={icon.svg} className="dev-icon" alt={icon.alt} />
+                            <h2>{icon.title}</h2>
+                        </div>
+                    )
+                })
+            }
+        </div>
     </div>
   )
 }
