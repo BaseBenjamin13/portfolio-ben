9c1a398506811dcfc181e5f3463cd672d68713b6
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-08-12 14:14:12 -0400

    added title and title animation with ityped

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { init } from 'ityped';
 import Resume from '../assets/imgs/Benjamin-Morgiewicz-GA.pdf';
 
 
 function Header() {
+
+    const titles = useRef();
+
+    useEffect(() => {
+        init(titles.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ['Engineer', 'Developer', 'Designer']
+          })
+    }, [])
     
   return (
     <div className="header">
         <div className="headerItem">
-            <a href="https://github.com/BaseBenjamin13" target="_blank">
+            <a href="https://github.com/BaseBenjamin13" target="_blank" rel="noreferrer">
                 <button className="btn-header-project">Github</button>
             </a>
         </div>
         <div className="headerItem">
-            <h1>Welcome To My Portfolio</h1>
+            <h1 id="welcome">Welcome To My Portfolio</h1>
             <p>Made By</p>
             <p>Including all projects</p>
-            <h2>Benjamin Morgiewicz</h2>
+            <h1>Benjamin Morgiewicz</h1>
+            <h1>The Software <span ref={titles}></span></h1>
             <h1>Email Me</h1>
             <a className="email-me" href="mailto:benmorgiewicz@gmail.com">benmorgiewicz@gmail.com</a>
-            <a className="resume" href={Resume} target="_blank">My Resume</a>
+            <a className="resume" href={Resume} target="_blank" rel="noreferrer">My Resume</a>
         </div>
         <div className="headerItem">
-            <a href="https://www.linkedin.com/in/benjamin-morgiewicz/" target="_blank">
+            <a href="https://www.linkedin.com/in/benjamin-morgiewicz/" target="_blank" rel="noreferrer">
                 <button className="btn-header-project">Linkedin</button>
             </a>
         </div>
